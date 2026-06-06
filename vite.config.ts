5904e6e0ed130c2992d267c495fef5a2be95dad7
Aissociate <meralli.ballou.s@gmail.com>
2026-06-06 09:01:16 +0400

    Updated package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  root: path.resolve(__dirname, 'web'),
   plugins: [react()],
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
